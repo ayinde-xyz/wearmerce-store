ebe0da8d62d7455e33ec2b42c64efc70832a29bc
ayinde-xyz <ayindeabdulrahman16@gmail.com>
2025-12-31 18:34:10 +0100

    last commit of 2026

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@ const { withNextVideo } = require("next-video/process");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "res.cloudinary.com",
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 module.exports = withNextVideo(nextConfig);
