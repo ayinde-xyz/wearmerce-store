0ea62bec7c3ee2cc45bf1e5299eb46147b285d60
ayinde-xyz <ayindeabdulrahman16@gmail.com>
2026-01-16 22:18:57 +0100

    added a figcaption to the banner

--- components/billboard/billboard2.tsx
+++ components/billboard/billboard2.tsx
@@ -6,32 +6,35 @@ const Billboard2 = () => {
     <div className="grid md:grid-cols-2 grid-cols-1">
       <figure className="relative w-full aspect-square">
         <Image
-          width={600}
-          height={600}
+          fill
           src={
             "https://res.cloudinary.com/dbgxwchuy/image/upload/v1734520474/sjvphyc61pidaoeke1cy.avif"
           }
           alt="Nike image"
           placeholder="blur"
-           blurDataURL="..."
+          blurDataURL="..."
         />
-        <figcaption className="absolute bottom-4 left-4 bg-white/70 px-3 py-1 rounded-md text-sm">
-          Nike Air Max 270 React
-          <Button className="ml-4 rounded-none">Shop Now</Button>
+        <figcaption className="absolute bottom-4 left-4  flex flex-col py-3 items-start px-3 space-y-2 rounded-md text-sm">
+          <p className="text-white">Manchester City Official Jersey</p>
+
+          <Button className="rounded-none">Shop Now</Button>
         </figcaption>
       </figure>
       <figure className="relative w-full aspect-square">
         <Image
-          width={1230}
-          height={1196}
+          fill
           src={
             "https://res.cloudinary.com/dbgxwchuy/image/upload/v1767719134/men-s-shoes-clothing-accessories_rjgcjf.avif"
           }
           alt=""
           placeholder="blur"
-           blurDataURL="..."
+          blurDataURL="..."
         />
-          <Button className="ml-4 rounded-none">Shop Now</Button>
+        <figcaption className="absolute bottom-4 left-4  flex flex-col py-3 items-start px-3 space-y-2 rounded-md text-sm">
+          <p className="text-white">Nike Air Max 400</p>
+
+          <Button className="rounded-none">Shop Now</Button>
+        </figcaption>
       </figure>
     </div>
   );
